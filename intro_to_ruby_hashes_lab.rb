fe62c2298f211d090bbd0de27d3d8c208a5b1e16
Sandra Wazaz <sandrawazaz@gmail.com>
2020-12-31 22:46:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ end
 
 def monopoly_with_third_tier
   {:railroads => {:pieces => 4, :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50,
-    :three_pieces_owned => 100, :four_pieces_owned => 200}, :names => {:reading_railroad => {:mortgage_value => 100},
+    :three_pieces_owned => 100, :four_pieces_owned => 200}, :names => {:reading_railroad => {},
     :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline_railroad => {}}
    }
   }
